ac01fd113e8d14deac2099bea141abe00c96586d
Lorenzo Usai <UsaiL@StPaulsSchool.org.uk>
2020-11-17 09:33:23 +0000

    Rename

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Stacks
+namespace Abstraction
 {
     class Program
     {
--- RPN.cs
+++ RPN.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Stacks
+namespace Abstraction
 {
     class Operations
     {
